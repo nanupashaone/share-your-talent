ee5476122cd21fd89ae30f543a7fc84ca61c896c
nanupashaone <billy.tab@hotmail.com>
2024-07-17 16:33:07 +0200

    design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Seo } from '@/ui/components/seo/seo';
+import { Button } from "@/ui/design-system/typography/button/button"
 import { Typography } from '@/ui/design-system/typography/typography';
 
 
@@ -7,8 +8,28 @@ export default function Home() {
     <>
       <Seo title="Share your talent" description="Description coming"/>
 
-      <Button />
+    <div className='flex items-center gap-4 p-10'>
+          <Button size='small'>Accent</Button>
+          <Button size='small' variant='secondary'>Secondary</Button>
+          <Button size='small' variant='outline'>Accent</Button>
+          <Button size='small' variant='disabled' disabled>Accent</Button>
 
+    </div>
+    <div className='flex items-center gap-4 p-10'>
+          <Button >Accent</Button>
+          <Button variant='secondary'>Secondary</Button>
+          <Button variant='outline'>Accent</Button>
+          <Button variant='disabled' disabled>Accent</Button>
+
+    </div>
+    <div className='flex items-center gap-4 p-10'>
+          <Button size='large'>Accent</Button>
+          <Button size='large' variant='secondary'>Secondary</Button>
+          <Button size='large' variant='outline'>Accent</Button>
+          <Button size='large' variant='disabled' disabled>Accent</Button>
+
+    </div>
+          
       {/*
       <div className='space-y-5'>
         <Typography theme='primary' variant='body-lg' component='h1' >
--- src/ui/design-system/typography/button/button.tsx
+++ src/ui/design-system/typography/button/button.tsx
@@ -1,9 +1,70 @@
+import clsx from 'clsx';
+import { Children } from 'react';
+import { isContext } from 'vm';
+
 interface Props {
-
+    size?: "small" | "medium" | "large";
+    variant?: "accent" | "secondary" | "outline" | "disabled" | "ico";
+    icon?: any;
+    iconTheme?: "accent" | "secondary" | "gray";
+    iconPosition?: "left" | "right";
+    disabled?: boolean;
+    isLoading?: boolean;
+    children?: React.ReactNode;
 }
 
-export const Button = ({}: Props) => {
-    return {
+export const Button = ({
+    size = 'medium',
+    variant = 'accent',
+    icon,
+    iconTheme = 'accent',
+    iconPosition = 'right',
+    disabled,
+    isLoading,
+    children,
+  }: Props) => {
 
-    };
-}
+    let variantStyles: string = "", 
+    sizeStyles: string = "", 
+    icoSize: number = 0;
+
+    switch (variant) {
+        case "accent": // Default style
+            variantStyles = "bg-primary hover:bg-primary-400 text-white rounded";
+            break;
+        case "secondary": 
+            variantStyles = "bg-primary-200 hover:bg-primary-300/50 text-primary rounded";
+            break;
+        case "outline": 
+            variantStyles = "bg-white hover:bg-gray-400/50 border border-gray-500 text-gray-900 rounded";
+            break;
+        case "disabled": 
+            variantStyles = "bg-gray-500 border-gray-500 text-gray-600 rounded cursor-not-allowed";
+            break;
+        case "ico": 
+            variantStyles = "";
+            break;
+    }
+
+    switch (size) {
+        case "small":
+            sizeStyles ="text-caption3 font-medium px-[14px] py-[12px]" ;
+            break;
+        case "medium": // Default style
+            sizeStyles ="text-caption2 font-medium px-[18px] py-[15px]";
+            break;
+        case "large":
+            sizeStyles ="text-caption1 font-medium px-[22px] py-[18px]";
+            break; 
+    }
+        
+
+   return  (
+    <>
+        <button 
+            type='button' className={clsx(variantStyles, sizeStyles, icoSize,"")} onClick={() => console.log("click")} disabled={disabled}>
+            {children}
+        </button>
+    </>
+   );
+};
